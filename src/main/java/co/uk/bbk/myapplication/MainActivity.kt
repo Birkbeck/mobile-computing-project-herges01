f7f2189c7bf44b6e74c6e90182c543c84184768d
Hashim Ergesous <herges01@student.bbk.ac.uk>
2025-06-01 10:43:00 +0100

    Recipe list page

--- src/main/java/co/uk/bbk/myapplication/MainActivity.kt
+++ src/main/java/co/uk/bbk/myapplication/MainActivity.kt
@@ -1,12 +1,23 @@
 package co.uk.bbk.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonBreakfast: ImageButton
+    private lateinit var buttonBrunch: ImageButton
+    private lateinit var buttonLunch: ImageButton
+    private lateinit var buttonDinner: ImageButton
+    private lateinit var buttonDessert: ImageButton
+    private lateinit var buttonOther: ImageButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Find the image button
+        buttonBreakfast = findViewById(R.id.imageButton1)
+        buttonBrunch = findViewById(R.id.imageButton2)
+        buttonLunch = findViewById(R.id.imageButton3)
+        buttonDinner = findViewById(R.id.imageButton4)
+        buttonDessert = findViewById(R.id.imageButton5)
+        buttonOther = findViewById(R.id.imageButton6)
+
+
+        buttonBreakfast.setOnClickListener {
+            val intent = Intent(this@MainActivity, RecipeListActivity::class.java)
+            startActivity(intent)
+        }
+        buttonBrunch.setOnClickListener {
+            val intent = Intent(this@MainActivity, RecipeListActivity::class.java)
+            startActivity(intent)
+        }
+        buttonLunch.setOnClickListener {
+            val intent = Intent(this@MainActivity, RecipeListActivity::class.java)
+            startActivity(intent)
+        }
+        buttonDinner.setOnClickListener {
+            val intent = Intent(this@MainActivity, RecipeListActivity::class.java)
+            startActivity(intent)
+        }
+        buttonDessert.setOnClickListener {
+            val intent = Intent(this@MainActivity, RecipeListActivity::class.java)
+            startActivity(intent)
+        }
+        buttonOther.setOnClickListener {
+            val intent = Intent(this@MainActivity, RecipeListActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
